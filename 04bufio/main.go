6c616037db4eee0df8396ed328ee8b388212589b
Student <student@mx23-hp>
2025-04-24 15:52:17 +0100

    third

--- 04bufio/main.go
+++ 04bufio/main.go
@@ -16,8 +16,9 @@ func main() {
 	// fmt.println(reflect.Typeof(name))
 	fmt.Print("Enter your favourite number")
 	fanNum, _ := reader.Readstring('/n')
-	fmt.Println("Your name is " favNum)
-	fmt.Print("Enter your fun fact")
-	funfact, _ := reader.ReadString('/n')
+	fmt.Println("Your name is ", favNum)
+		fmt.Print("Enter your fun fact")
+	funfact, _ := reader.ReadString('\n')
 	fmt.Println("Your fun fact is ", funFact)  
+fmt.Println("")
 }
---
+++ task/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	
+	var name string = "james clear"
+	var favourite book string = "atomic habits"
+	var year book publish int = 2018
+
+	fmtPrintf("the-author-of-book %v the-year-of-publish:
+	
+}
